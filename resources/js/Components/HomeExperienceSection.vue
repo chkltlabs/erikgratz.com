2c80218195ad1fa9ccbe64d5c3d6a08da4e63e0b
chkltlabs <chkltlabs@gmail.com>
2022-05-30 09:31:26 -0700

    experience update

--- resources/js/Components/HomeExperienceSection.vue
+++ resources/js/Components/HomeExperienceSection.vue
@@ -5,22 +5,39 @@
             <div class="flex flex-col lg:flex-row justify-between">
                 <div class="  mb-16 md:mb-0">
                     <h6 class="font-medium text-gray-400 text-base uppercase">Company</h6>
+                    <p v-for="jerb, key in jerbs" 
+                        v-bind:key="key" 
+                        class="font-semibold text-gray-600 text-base"
+                        >{{jerb.company}}
+                        <span class="font-normal text-gray-300">/ {{jerb.location}}</span>
+                    </p>
+                    <!-- <p class="font-semibold text-gray-600 text-base">Pocketnest
+                        <span class="font-normal text-gray-300">/ Detroit, MI -> Fully Remote</span>
+                    </p>
                     <p class="font-semibold text-gray-600 text-base">Sonic Boom Wellness
                         <span class="font-normal text-gray-300">/ San Diego, CA -> Fully Remote</span>
                     </p>
                     <p class="font-semibold text-gray-600 text-base">Internet Things -> The Media Lab
                         <span class="font-normal text-gray-300">/ San Diego, CA</span>
-                    </p>
+                    </p> -->
                 </div>
                 <div class="  mb-16 md:mb-0">
                     <h6 class="font-medium text-gray-400 text-base uppercase">Position</h6>
+                    <p v-for="jerb, key in jerbs" 
+                        v-bind:key="key"
+                        class="font-normal text-gray-400 text-base">{{jerb.title}}</p>
+                    <!-- <p class="font-normal text-gray-400 text-base">Senior Software Engineer, Backend</p>
                     <p class="font-normal text-gray-400 text-base">Backend Developer</p>
-                    <p class="font-normal text-gray-400 text-base">PHP Developer -> Lead PHP Developer</p>
+                    <p class="font-normal text-gray-400 text-base">PHP Developer -> Lead PHP Developer</p> -->
                 </div>
                 <div class="">
                     <h6 class="font-medium text-gray-400 text-base uppercase">Year</h6>
-                    <p class="font-normal text-gray-400 text-base">2020-{{ new Date().getFullYear() }}</p>
-                    <p class="font-normal text-gray-400 text-base">2019-2020</p>
+                    <p v-for="jerb, key in jerbs" 
+                        v-bind:key="key"
+                        class="font-normal text-gray-400 text-base">{{jerb.timeframe}}</p>
+                    <!-- <p class="font-normal text-gray-400 text-base">2022-{{ new Date().getFullYear() }}</p>
+                    <p class="font-normal text-gray-400 text-base">2020-2022</p>
+                    <p class="font-normal text-gray-400 text-base">2019-2020</p> -->
                 </div>
             </div>
         </div>
@@ -32,7 +49,15 @@
                 <div class="space-y-8 lg:space-y-16 mb-16 md:mb-0">
                     <h6 class="font-medium text-gray-300 text-base">
                         Company / Location <br class="md:hidden">/ Title / Timeframe</h6>
-                    <p class="font-semibold text-gray-300 text-base">Sonic Boom Wellness
+                    <p v-for="jerb, key in jerbs" 
+                        v-bind:key="key"
+                        class="font-semibold text-gray-300 text-base">{{jerb.company}}
+                        <span class="font-normal text-gray-500">/ {{jerb.location}} </span>
+                        <br class="md:hidden"/>
+                        <span class="font-normal text-gray-300">/ {{jerb.title}} </span>
+                        <span class="font-normal text-gray-500">/ {{jerb.timeframe}}</span>
+                    </p>
+                    <!-- <p class="font-semibold text-gray-300 text-base">Sonic Boom Wellness
                         <span class="font-normal text-gray-500">/ San Diego, CA -> Fully Remote </span>
                         <br class="md:hidden"/>
                         <span class="font-normal text-gray-300">/ Backend Developer </span>
@@ -43,7 +68,7 @@
                         <br class="md:hidden"/>
                         <span class="font-normal text-gray-500">/ PHP Developer -> Lead PHP Developer </span>
                         <span class="font-normal text-gray-300">/ 2019-2020</span>
-                    </p>
+                    </p> -->
                 </div>
             </div>
         </div>
@@ -51,6 +76,32 @@
 </template>
 <script>
 
-export default {}
+export default {
+data() {
+        return {
+            jerbs: [
+                {
+                    company: 'Pocketnest',
+                    location: 'Detroit, MI -> Fully Remote',
+                    title: 'Senior Software Engineer, Backend',
+                    timeframe: '2022-' + new Date().getFullYear() ,
+                },
+                {
+                    company: 'Sonic Boom Wellness',
+                    location: 'San Diego, CA -> Fully Remote',
+                    title: 'Backend Developer',
+                    timeframe: '2020-2022',
+                },
+                {
+                    company: 'Internet Things -> The Media Lab',
+                    location: 'San Diego, CA',
+                    title: 'PHP Developer -> Lead PHP Developer',
+                    timeframe: '2019-2020',
+                }
+            ]
+        }
+    }
+    
+}
 
 </script>
