94e7a8d71f46b591cceba9f83eeff99f4d195ab1
chkltlabs <chkltlabs@gmail.com>
2024-06-28 14:22:12 +0300

    bugfix: dolla signs

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -35,10 +35,10 @@ class StatsOverview extends BaseWidget
             ->sum('amount');
 
         return [
-            Stat::make('Current Month Unpaid', $curM),
-            Stat::make('Next Month Unpaid', $nexM),
-            Stat::make('90 Days Unpaid', $day90),
-            Stat::make('180 Days Unpaid', $day180)
+            Stat::make('Current Month Unpaid', '$'.$curM),
+            Stat::make('Next Month Unpaid', '$'.$nexM),
+            Stat::make('90 Days Unpaid', '$'.$day90),
+            Stat::make('180 Days Unpaid', '$'.$day180)
         ];
     }
 }
