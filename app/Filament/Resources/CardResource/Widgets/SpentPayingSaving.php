e8e608a0306edfe8dbc8ac83a20ba9241cb07720
chkltlabs <chkltlabs@gmail.com>
2024-08-25 21:12:30 +0300

    feature : Cards Widgets, user monthly pay,

---
+++ app/Filament/Resources/CardResource/Widgets/SpentPayingSaving.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\CardResource\Widgets;
+
+use App\Models\Card;
+use App\Models\User;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class SpentPayingSaving extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $reusedQuery = Card::where('due_date', '>=', now()->day);
+        $thisMonth = $reusedQuery->sum('interest_saving_balance');
+        $nextMonth = (
+            Card::where('due_date', '<', now()->day)->sum('interest_saving_balance')
+            - $thisMonth
+            + $reusedQuery->sum('balance')
+            + $reusedQuery->sum('pending')
+        );
+        $potentialSave = User::sum('monthly_pay') - $nextMonth;
+        return [
+            Stat::make('This Month Spend', $thisMonth),
+            Stat::make('Next Month Spend', $nextMonth),
+            Stat::make('Next Month Save', $potentialSave),
+        ];
+    }
+}
