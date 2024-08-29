9f18e0ec9e66b562c7ee2898ed9336e207590849
chkltlabs <chkltlabs@gmail.com>
2024-08-29 16:41:25 +0300

    feature : Abbreviated dash tables,

--- app/Filament/Widgets/AccountWidget.php
+++ app/Filament/Widgets/AccountWidget.php
@@ -6,17 +6,30 @@ use App\Filament\Resources\AccountResource;
 use App\Models\Account;
 use Filament\Forms\Form;
 use Filament\Tables;
+use Filament\Tables\Columns\Summarizers\Sum;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Model;
 
 class AccountWidget extends BaseWidget
 {
-    protected int | string | array $columnSpan = 'full';
+    protected int | string | array $columnSpan = '0.5';
 
     protected static ?string $heading = 'Accounts';
     public function table(Table $table): Table
     {
-        return AccountResource::table($table->query(Account::query()))
-            ->paginated(false);
+        return $table->query(Account::query())
+            ->paginated(false)
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->description(fn (Model $record)
+                    => $record->updated_at->shortRelativeDiffForHumans(),
+                        'below'),
+                Tables\Columns\TextInputColumn::make('balance')
+                    ->rules(['numeric'])
+                    ->summarize(Sum::make()->money()->label(''))
+                    ->sortable()
+            ])
+            ;
     }
 }
